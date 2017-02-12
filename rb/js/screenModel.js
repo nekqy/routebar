ab3b2e76515f2af381dbe5f89d1b0883bffbef81
nekqy <nekqy@yandex.ru>
2017-02-12 03:28:12 +0300

    screenModel api extended.

--- rb/js/screenModel.js
+++ rb/js/screenModel.js
@@ -1,7 +1,7 @@
 define([], function() {
     "use strict";
 
-    function Screen(html, children, isPermanent, isDirectedGraph) {
+    function Screen(html, children, isPermanent, isDirectedGraph, doChildrenCyclic, doParentsCyclic) {
         if (!Screen._mainScreenSetted) {
             Screen._mainScreen = this;
         }
@@ -12,11 +12,15 @@ define([], function() {
         this._id = 'screen_' + Screen._length++;
 
         if (Array.isArray(html)) {
+            doParentsCyclic = html[4];
+            doChildrenCyclic = html[4];
             isDirectedGraph = html[3];
             isPermanent = html[2];
             children = html[1];
             html = html[0];
         } else if (typeof html === 'object') {
+            doParentsCyclic = html.doParentsCyclic;
+            doChildrenCyclic = html.doChildrenCyclic;
             isDirectedGraph = html.isDirectedGraph;
             isPermanent = html.isPermanent;
             children = html.children;
@@ -25,10 +29,19 @@ define([], function() {
             html = '';
         }
 
-        this.html = html;
-        this._temporary = !isPermanent;
-        this._isDirectedGraph = !!isDirectedGraph;
+        if (doChildrenCyclic === undefined) {
+            doChildrenCyclic = true;
+        }
+        if (doParentsCyclic === undefined) {
+            doParentsCyclic = true;
+        }
+
+        this.html = html; // TODO на изменение html изменять элементы
         this.setChildren(children);
+        this._temporary = !isPermanent; // Todo на изменение тут менять и там где используется
+        this._isDirectedGraph = !!isDirectedGraph; // Todo на изменение тут менять и там где используется
+        this.doChildrenCyclic(doChildrenCyclic);
+        this.doParentsCyclic(doParentsCyclic);
     }
     Screen.prototype.toString = function() {
         return this._id;
@@ -36,14 +49,59 @@ define([], function() {
     Screen.prototype.isTemporary = function() {
         return this._temporary;
     };
-    Screen.prototype.setChildren = function(children) {
+    Screen.prototype.isDirectedGraph = function() {
+        return this._isDirectedGraph;
+    };
+
+    Screen.prototype._addScreen = function(screen, arr, oppositeArr, doCyclic) {
+        if (!this._isDirectedGraph) {
+            oppositeArr.push(this);
+        }
+        if (doCyclic) {
+            if (arr.length === 0) {
+                screen._next = null; // TODO может = screen?
+                screen._prev = null;
+            } else {
+                screen._next = arr.length ? arr[0] : screen;
+                screen._prev = arr.length ? arr[arr.length-1] : screen;
+                screen._next._prev = screen;
+                screen._prev._next = screen;
+            }
+        } else {
+            if (arr.length === 0) {
+                screen._next = null;
+                screen._prev = null;
+            } else {
+                screen._next = null;
+                screen._prev = arr[arr.length-1];
+                screen._prev._next = screen;
+            }
+        }
+
+        arr.push(screen);
+        Screen._runRelativeUpdateFn(this);
+        return this;
+    };
+    Screen.prototype.addChild = function(child) {
+        return this._addScreen(child, this._children, child._parents, this._doChildrenCyclic);
+    };
+    Screen.prototype.addParent = function(parent) {
+        return this._addScreen(parent, this._parents, parent._children, this._doParentsCyclic);
+    };
+    Screen.prototype.addChildren = function(children) {
         if (children && Array.isArray(children)) {
-            this._children = [];
             for (var i = 0; i < children.length; i++) {
                 this.addChild(children[i]);
             }
-        } else {
-            this._children = [];
+        }
+        Screen._runRelativeUpdateFn(this);
+        return this;
+    };
+    Screen.prototype.addParents = function(parents) {
+        if (parents && Array.isArray(parents)) {
+            for (var i = 0; i < parents.length; i++) {
+                this.addParent(parents[i]);
+            }
         }
         Screen._runRelativeUpdateFn(this);
         return this;
@@ -51,31 +109,33 @@ define([], function() {
     Screen.prototype.getChildren = function() {
         return this._children;
     };
-    Screen.prototype.addChild = function(child) {
-        var children = this._children;
-        if (!this._isDirectedGraph) {
-            child._parents.push(this);
-        }
-        if (children.length > Screen._doCyclic) {
-            child._next = children.length ? children[0] : child;
-            child._prev = children.length ? children[children.length-1] : child;
-            child._next._prev = child;
-            child._prev._next = child;
-        }
-
-        children.push(child);
-        Screen._runRelativeUpdateFn(this);
-        return this;
+    Screen.prototype.getParents = function() {
+        return this._parents;
     };
-    Screen.prototype.removeChild = function(child) {
-        var children = this._children,
-            index = children.indexOf(child),
+    Screen.prototype.getChild = function(id) {
+        return this._children.find(function(child) { // TODO полифилл!
+            return child.toString() === id;
+        });
+    };
+    Screen.prototype.getParent = function(id) {
+        return this._parents.find(function(parent) { // TODO полифилл!
+            return parent.toString() === id;
+        });
+    };
+    Screen.prototype.setChildren = function(children) {
+        return this.clearChildren().addChildren(children);
+    };
+    Screen.prototype.setParents = function(parents) {
+        return this.clearParents().addParents(parents);
+    };
+    Screen.prototype._removeScreen = function(screen, arr) {
+        var index = arr.indexOf(screen),
             removed;
         if (index !== -1) {
-            removed = children.splice(index, 1)[0];
+            removed = arr.splice(index, 1)[0];
 
             if (removed._prev === removed._next) {
-                remove._prev = remove._next = null;
+                removed._prev = removed._next = null;
             } else {
                 removed._prev._next = removed._next;
                 removed._next._prev = removed._prev;
@@ -85,19 +145,55 @@ define([], function() {
         Screen._runRelativeUpdateFn(this);
         return this;
     };
-    Screen.prototype.doChildrenCyclic = function(val) {
-        if (this._children.length > 0) {
+    Screen.prototype.removeChild = function(child) {
+        return this._removeScreen(child, this._children);
+    };
+    Screen.prototype.removeParent = function(parent) {
+        return this._removeScreen(parent, this._parents);
+    };
+    Screen.prototype._clearScreens = function(arr) {
+        var child;
+        if (arr && Array.isArray(arr)) {
+            for (var i = 0; i < arr.length; i++) {
+                child = arr[i];
+                child._prev = child._next = null;
+            }
+        }
+    };
+    Screen.prototype.clearChildren = function() {
+        this._clearScreens(this._children);
+        this._children = [];
+        Screen._runRelativeUpdateFn(this);
+        return this;
+    };
+    Screen.prototype.clearParents = function() {
+        this._clearScreens(this._parents);
+        this._parents = [];
+        Screen._runRelativeUpdateFn(this);
+        return this;
+    };
+
+    Screen.prototype.doChildrenCyclic = function(doCyclic) {
+        this._doChildrenCyclic = !!doCyclic;
+        this._doCyclic(doCyclic, this._children);
+    };
+    Screen.prototype.doParentsCyclic = function(doCyclic) {
+        this._doParentsCyclic = !!doCyclic;
+        this._doCyclic(doCyclic, this._parents);
+    };
+    Screen.prototype._doCyclic = function(val, arr) {
+        if (arr.length > 0) {
             if (val) {
-                if (this._children.length > 1) {
-                    this._children[0]._prev = this._children[this._children.length-1];
-                    this._children[this._children.length-1]._next = this._children[0];
+                if (arr.length > 1) {
+                    arr[0]._prev = arr[arr.length-1];
+                    arr[arr.length-1]._next = arr[0];
                 } else {
-                    this._children[0]._prev = null;
-                    this._children[this._children.length-1]._next = null;
+                    arr[0]._prev = null;
+                    arr[arr.length-1]._next = null;
                 }
             } else {
-                this._children[0]._prev = null;
-                this._children[this._children.length-1]._next = null;
+                arr[0]._prev = null;
+                arr[arr.length-1]._next = null;
             }
         }
     };
@@ -126,10 +222,6 @@ define([], function() {
         Screen._relativeUpdateFn.forEach(function(fn) {
             fn.call(undefined, screen);
         });
-    };
-    Screen._doCyclic = 0;
-    Screen.doCyclic = function(val) {
-        Screen._doCyclic = val ? 0 : 1;
     };
 
     return Screen;
